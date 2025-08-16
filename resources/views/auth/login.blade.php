9843606c239cb82f408c94c98e55bbbcbea255dc
Lakshan Dissanayake <IT20197414@my.sliit.lk>
2025-08-16 16:25:24 +0530

    Final commit I think

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,3 +1,4 @@
+@php($pageTitle = 'Login')
 <x-guest-layout>
     <!-- Session Status -->
     <x-auth-session-status class="mb-4" :status="session('status')" />
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,3 +1,4 @@
+@php($pageTitle = 'Register')
 <x-guest-layout>
     <form method="POST" action="{{ route('register') }}">
         @csrf
